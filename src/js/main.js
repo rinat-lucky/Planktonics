da7ebe4e26ad3390e818e7106331a3d1c1907f0e
Rinat <rinat1kam@mail.ru>
2021-02-28 18:06:15 +0500

    авторизация + выход

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,18 @@
-const userNameElem = document.querySelector('.user-name');
+const loginElem = document.querySelector('.login');
+const loginForm = document.querySelector('.login-form');
+const loginForget = document.querySelector('.login-forget');
+const loginSignup = document.querySelector('.login-signup');
+const emailInput = document.querySelector('.login-email');
+const regExpValidEmail = /^\w+@\w+\.\w{2,}$/;
+const userElem = document.querySelector('.user');
 const userAvatarElem = document.querySelector('.user-avatar');
+const DEFAULT_PHOTO = userAvatarElem.src;
+const userNameElem = document.querySelector('.user-name');
+const exitElem = document.querySelector('.exit');
+const editElem = document.querySelector('.edit');
+const editContainer = document.querySelector('.edit-container');
+const editUserName = document.querySelector('.edit-username');
+const editPhotoURL = document.querySelector('.edit-photo');
 const postsWrapper = document.querySelectorAll('.post-wrapper');
 const addPostElem = document.querySelector('.add-post');
 let tabs = document.querySelectorAll('.sidebar-menu-link');
@@ -13,11 +26,9 @@ function hideTabContent() {
     tabsContent.forEach(item => {
         item.style.display = 'none';
     });
-
     tabs.forEach(item => {
         item.classList.remove('active');
     });
-	
 	postsWrapper.forEach(item => {
         item.classList.remove('post-wrapper-active');
     });
@@ -44,29 +55,55 @@ tabsParent.addEventListener('click', function(event) {
 	}
 });
 
-
 // Добавление сообщений в чат (и удаление авторских сообщений)
+
+const setUsers = {
+	user: {
+		name: '',
+		photo: ''
+	},
+	logIn(email) {
+		if (!regExpValidEmail.test(email)) {
+		  	alert('Некорректный e-mail');
+		  	return;
+		} else {
+			setUsers.user.name = email.substring(0, email.indexOf('@'));
+			toggleAuthDom();
+		}
+	},
+	logOut() {
+		setUsers.user = undefined;
+		toggleAuthDom();
+	},
+	editUser(nameEdit, photoEdit) {
+		if (nameEdit) {
+			setUsers.user.name = nameEdit;
+		}
+		if (photoEdit) {
+			setUsers.user.photo = photoEdit;
+		}		
+		toggleAuthDom();
+	},
+};
 
 const setPosts = {
 	allPosts: [],
 	addPost(title, text, handler) {
-		
 		this.allPosts.unshift({
 			title, 
 			text, 
 			date: new Date().toLocaleString(), 
 			likes: 0
 		});
-
 		if (handler) {
 			handler();
 		}
 	},
 };
 
-const showAllPosts = () => {
-	const userAvatar = userAvatarElem.src;
-	const userName = userNameElem.textContent;
+const showAllPosts = function () {
+	const userAvatar = setUsers.user.photo;
+	const userName = setUsers.user.name;
 	const title = addPostElem.elements.title.value;
 	const text = addPostElem.elements.text.value;
 	const date = new Date().toLocaleString();
@@ -106,7 +143,22 @@ const showAllPosts = () => {
 
 	const serializedPosts = JSON.stringify(setPosts.allPosts);
 	localStorage.setItem('all posts', serializedPosts);
-	
+
+	// const serializedPosts = document.querySelectorAll('.post');
+	// serializedPosts.forEach(function(item, i) { 
+	// 	const serializedPostAuthor = JSON.stringify(`${item[i].userName}`);
+	// 	const serializedPostContent = JSON.stringify(`${item[i].title}: ${item[i].text}`);
+	// 	localStorage.setItem(serializedPostAuthor, serializedPostContent);
+	// });
+
+
+	// area.value = localStorage.getItem('area');
+    // area.oninput = () => {
+    //   localStorage.setItem('area', area.value)
+    // };
+
+
+
 	const deleteBtns = document.querySelectorAll('.delete');
 	deleteBtns.forEach((item) => {
 		item.addEventListener('click', () => {
@@ -114,11 +166,49 @@ const showAllPosts = () => {
 			post.remove();
 		});
 	});
-
 	return;
 };
 
+const toggleAuthDom = () => {
+	const user = setUsers.user;
+	if (user) {
+	  	loginElem.style.display = 'none';
+	  	userElem.style.display = 'block';
+	  	userNameElem.textContent = user.name;
+	  	userAvatarElem.src = user.photo || DEFAULT_PHOTO;
+		addPostElem.classList.add('visible');
+		// localStorage.setItem('users', serializedUsers);
+	} else {
+	  	loginElem.style.display = '';
+	  	userElem.style.display = 'none';
+	  	addPostElem.classList.remove('visible');
+	}
+};
+
 const init = () => {
+	loginForm.addEventListener('submit',() => {
+		event.preventDefault();
+		setUsers.logIn(emailInput.value);
+		loginForm.reset();
+	});
+	loginSignup.addEventListener('click', () => {
+		event.preventDefault();
+		loginForm.reset();
+		alert('Здесь должен запускаться скрипт регистрации пользователя');
+	});
+	exitElem.addEventListener('click', () => {
+		event.preventDefault();
+		setUsers.logOut();
+	});
+	editElem.addEventListener('click', () => {
+		event.preventDefault();
+		editContainer.classList.toggle('visible');
+	});
+	editContainer.addEventListener('submit', () => {
+		event.preventDefault();
+		editContainer.classList.remove('visible');
+		setUsers.editUser(editUserName.value, editPhotoURL.value);
+	});
 	addPostElem.addEventListener('submit', () => {
 		event.preventDefault();
 		const { title, text } = addPostElem.elements;
@@ -127,7 +217,6 @@ const init = () => {
 			alert('Слишком короткий заголовок сообщения');
 			return;
 		}
-
 		if (text.value.length < 3) {
 			alert('Слишком короткое сообщение');
 			return;
@@ -137,5 +226,10 @@ const init = () => {
 		addPostElem.reset();
 	});
 };
+
+loginForget.addEventListener('click', () => {
+	event.preventDefault();
+	alert('Здесь должен запускаться скрипт восстановления пароля через e-mail');
+});
 
 document.addEventListener('DOMContentLoaded', init);
